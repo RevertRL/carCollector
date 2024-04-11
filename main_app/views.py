f9508ffa062df71151cd1a8e43b78e4e6929d74c
Eric Fargo <fargo10k@gmail.com>
2024-04-11 12:08:09 -0400

    carCollector setup

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+cars = [
+    {'make': 'Dodge', 'model': 'Charger', 'year': '1969', 'description': 'General Lee'},
+    {'make': 'Plymouth', 'model': 'Road Runner', 'year': '1968', 'description': 'Best Road Runner year'},
+    {'make': 'Dodge', 'model': 'Charger Daytona', 'year': '1973', 'description': 'Richard Pettys'},
+]
+
+
+def home(request):
+  return render(request, 'home.html')
+
+def about_view(request):
+    return render(request, 'about.html')
+
+def cars_index(request):
+    return render(request, 'cars/index.html', {
+    'cars': cars
+})
