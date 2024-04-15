b3352d1368f5a24b60ddd8fd453dcfa1e631aeae
Eric Fargo <fargo10k@gmail.com>
2024-04-15 16:30:08 -0400

    parts 3-4 done

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 from django.urls import reverse
 # Create your models here.
+
+
+CARSHOWS = (
+    ('Chi' , 'Chicago Auto Show'),
+    ('NAi', 'North American International Auto Show'),
+    ('SFi', 'SanFrancisco International Auto Show'),
+    ('NYi', 'New York International Auto Show'),
+    ('GNR', 'Grand National Roadster Show')
+)
+
+
+
 class Car( models.Model):
     make = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
@@ -15,3 +27,15 @@ class Car( models.Model):
     def get_absolute_url(self):
         return reverse("detail", kwargs={'car_id': self.id})
     
+class Carshow(models.Model):
+    date = models.DateField()
+    location = models.CharField(max_length=30)
+    carshow = models.CharField(
+        max_length=30, 
+            choices=CARSHOWS,
+            default=CARSHOWS[0][0]
+        )
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.get_carshow_display()} on {self.date}"
